81a49b02c853c67f68c7a8a65fb6886a46c4a79a
RongxinRuan <1178102633@qq.com>
2021-11-14 14:10:43 +0800

    修改 对密码进行加密

--- sequelize/index.js
+++ sequelize/index.js
@@ -1,9 +1,14 @@
 require('./model/relations');
 // require('./mock/mockStudent');
 
-const studentServe = require('./services/studentService');
-const bookServe = require('./services/bookService');
-const classServe = require('./services/classService')
+// const studentServe = require('./services/studentService');
+// const bookServe = require('./services/bookService');
+// const classServe = require('./services/classService');
+const adminServe = require('./services/adminService');
+
+adminServe.login('wangnima', 'wangnima').then((res) => {
+  console.log(res)
+})
 // studentServe.getStudents(1, 100, 1, '超').then((res) => {
 //   console.log(res);
 // })
@@ -12,6 +17,6 @@ const classServe = require('./services/classService')
 //   console.log(res);
 // })
 
-classServe.getClasses().then((res) => {
-  console.log(res);
-})
+// classServe.getClasses().then((res) => {
+//   console.log(res);
+// })
--- sequelize/services/adminService.js
+++ sequelize/services/adminService.js
@@ -1,6 +1,7 @@
 const Admin = require('../model/Admin');
-
+const md5 = require('js-md5');
 exports.addAdmin = async (adminObj) => {
+  adminObj.loginPwd = md5(adminObj.loginPwd);
   // 此处省略各种校验逻辑 直接做添加
   const ins = await Admin.create(adminObj);
   return ins.toJSON();
@@ -16,6 +17,7 @@ exports.deleteAdmin = async (adminId) => {
 }
 
 exports.updateAdmin = async (adminObj, id) => {
+  adminObj.loginPwd = md5(adminObj.loginPwd);
   const result = await Admin.update(adminObj, {
     where: {
       id,
@@ -28,7 +30,7 @@ exports.login = async (loginId, loginPwd) => {
   const result = await Admin.findOne({
     where: {
       loginId,
-      loginPwd
+      loginPwd: md5(loginPwd)
     }
   })
   // 判断大小写是否一致
